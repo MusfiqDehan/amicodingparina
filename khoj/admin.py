b11fad0c4e306758cc1242995abd088b08599076
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-07-24 21:24:41 +0600

    Update database

--- khoj/admin.py
+++ khoj/admin.py
@@ -7,7 +7,7 @@ admin.site.index_title = "Welcome to Ami Coding Pari Na Admin Panel"
 
 
 class InputAdmin(admin.ModelAdmin):
-    list_display = ('user', 'input_values', 'timestamp')
+    list_display = ('user', 'input_values', 'timestamp', 'user_id')
 
 
 admin.site.register(Input, InputAdmin)
