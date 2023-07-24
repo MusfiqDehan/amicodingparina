440c8add7f69a83d299438e8c6d948ebba270fe1
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-07-24 15:41:05 +0600

    Add and update admin.py

--- khoj/admin.py
+++ khoj/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.site_header = "Ami Coding Pari Na Admin"
+admin.site.site_title = "Ami Coding Pari Na"
+admin.site.index_title = "Welcome to Ami Coding Pari Na Admin Panel"
+
+
+class InputAdmin(admin.ModelAdmin):
+    list_display = ('user', 'input_values', 'timestamp')
+
+
+admin.site.register(Input, InputAdmin)
